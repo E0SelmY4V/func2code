851405a86783faa815dc14cfc5ddfa1ebbe789fa
E0SelmY4V-PE <yabo1887415157@qq.com>
2023-07-04 23:14:40 +0800

    Env 使用Tape进行单元测试

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 const func2code = require('..');
 const checkList = require('./fn');
+const test = require('tape');
 
 const R = {
 	params: [],
@@ -13,36 +14,16 @@ const R = {
 	isGenerator: false
 };
 
-function isObjectEqual(obj1, obj2) {
-	let o1 = obj1 instanceof Object, o2 = obj2 instanceof Object;
-	if (!o1 || !o2) return obj1 === obj2;
-	if (Object.keys(obj1).length !== Object.keys(obj2).length) return false;
-	for (let attr in obj1) {
-		let a1 = Object.prototype.toString.call(obj1[attr]) == '[object Object]',
-			a2 = Object.prototype.toString.call(obj2[attr]) == '[object Object]',
-			ar = Object.prototype.toString.call(obj1[attr]) == '[object Array]';
-		if (a1 && a2) return isObjectEqual(obj1[attr], obj2[attr]);
-		else if (ar) {
-			if (obj1[attr].toString() != obj2[attr].toString()) return false;
-		} else if (obj1[attr] !== obj2[attr]) return false;
-	}
-	return true;
-};
-
 var c = 0;
 for (let [f, r, e] of checkList) {
 	if (typeof f === 'object') for (const i in f) f = f[i];
 	const splited = func2code.split(f);
 	for (const i in R) i in r || (r[i] = R[i]);
-	if (!isObjectEqual(splited, r)) {
-		console.log('应', r);
-		// console.log(f.toString());
-		console.log('实', splited);
-		console.log('\33[31m--------------------\33[39m');
-		console.log(c);
-		throw e;
-	}
+	test(`fn${c}`, t => {
+		t.deepEqual(splited, r, e.stack);
+		t.end();
+	});
 	c++;
 }
 
-require('./import');
+//require('./import');
